bc9a7017a055042124efa93fa925e800ddb18ec4
GazetaGaveta <74062534+GazetaGaveta@users.noreply.github.com>
2022-04-02 20:35:13 -0300

    commit final. provavelmente...

--- src/main/java/com/testessys/ApiSyss/ApiSsysApplication.java
+++ src/main/java/com/testessys/ApiSyss/ApiSsysApplication.java
@@ -2,15 +2,14 @@ package com.testessys.ApiSyss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication//(exclude = {SecurityAutoConfiguration.class})
-public class ApiSyssApplication {
+public class ApiSsysApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiSyssApplication.class, args);
+		SpringApplication.run(ApiSsysApplication.class, args);
 	}
 
 	@Bean
